3a042e465b8e8b78cda24875b25521907ba67db0
yfxfxxxfxx <mail@przykladowy.co.pl>
2020-04-18 16:01:09 +0200

    .

--- src/main/java/pl/zdejme/api/init/ImageInitializer.java
+++ src/main/java/pl/zdejme/api/init/ImageInitializer.java
@@ -43,8 +43,8 @@ public class ImageInitializer implements CommandLineRunner {
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
 
         body.put("files", List.of(
-                ImageInitializer.class.getResourceAsStream("/init/GettyImages-142116239_medium.jpg")
-//                new ClassPathResource("init\\GettyImages-142116239_medium.jpg", this.getClass().getClassLoader())
+//                ImageInitializer.class.getResourceAsStream("/init/GettyImages-142116239_medium.jpg")
+                new ClassPathResource("init\\GettyImages-142116239_medium.jpg", this.getClass().getClassLoader())
 //                new ClassPathResource("src\\main\\resources\\init\\orca.jpg"),
 //                new ClassPathResource("src\\main\\resources\\init\\pacnw.jpg"),
 //                new ClassPathResource("src\\main\\resources\\init\\platonov.png"),
